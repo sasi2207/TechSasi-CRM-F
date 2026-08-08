d92bfec28b11920af8b9ae19477e022f8e2ee0f5
sasi2207 <sasikumarp2207@gmail.com>
2026-08-08 20:00:05 +0530

    Retry GitHub Actions18

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // const BACKEND_URL = "http://localhost:8000";
 
 
-const BACKEND_URL = "https://crm.techsasi.com";
+const BACKEND_URL = "https://api.techsasi.com";
 
 export const API = `${BACKEND_URL}/api`;
 
